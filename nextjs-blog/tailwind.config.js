20b2ca3b24698ac267a880eb82adb40183bca972
DarioFD <dfungueirinhoduro3@gmail.com>
2023-08-04 13:24:17 +0200

    TailwindWorking

--- nextjs-blog/tailwind.config.js
+++ nextjs-blog/tailwind.config.js
@@ -1,13 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false,
   theme: {
+    extend: {
+      colors: {
+        primary: '#FF0000',
+        secondary: '#00FF00',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'Arial', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
-
+};
