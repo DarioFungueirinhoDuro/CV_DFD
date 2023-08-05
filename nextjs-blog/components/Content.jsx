08ee49f9a07c9e9388eff8de5fd71bad3c4c2b60
DFD <dfungueirinhoduro3@gmail.com>
2023-08-05 14:04:08 +0200

    ChangeColorEvent

--- nextjs-blog/components/Content.jsx
+++ nextjs-blog/components/Content.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import 'tailwindcss/tailwind.css';
 export default function Navbar(){
     return (
-        <div style={{minHeight:'90%' ,zIndex:"2" , marginTop:"-15px",paddingTop:"30px",backgroundColor:"orange",position:"relative"}}>Content</div>
+        <div id="content" style={{minHeight:'90%' ,zIndex:"2" , marginTop:"-15px",paddingTop:"30px",backgroundColor:"orange",position:"relative"}} className="p-5">Content</div>
     );
 }
--- nextjs-blog/components/LabelBar.jsx
+++ nextjs-blog/components/LabelBar.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import 'tailwindcss/tailwind.css';
 export default function LabelBar(input) {
     var changeLabel = function () { 
-        alert('Label change '+ input.color);
+        var color = input.color;
+        var zIndex = input.zIndex;
+        var actualColor = document.getElementById('label'+zIndex).style.backgroundColor;
+        var actualIndex = document.getElementById('label' + zIndex).style.zIndex;
+        for (var i = 0; i <= 3; i++) {
+            if (actualIndex > 3) {
+                document.getElementById('label0').style.zIndex = actualIndex - 5;
+            }
+        }
+        document.getElementById('content').style.backgroundColor = color;
+        document.getElementById('label'+input.zIndex).style.zIndex = zIndex + 5;
     }
     var width = 100 * ((4 - parseInt(input.zIndex)) / 4);
     var marginLeft = input.zIndex == 3 ? 0 : (-15);
@@ -13,8 +23,9 @@ export default function LabelBar(input) {
         zIndex: `${input.zIndex}`,
         marginLeft: `${marginLeft}px` 
     }
-    return (<div style={mystyle} className="flex flex-row justify-end content-start rounded-2xl">
+    return (<div id={"label"+input.zIndex} style={mystyle} className="flex flex-row justify-end content-start rounded-2xl">
         <button style={{ width: "100%", height: "100%"}} onClick={changeLabel}>
-            <h3>Cosas {input.color}</h3></button>
-            </div>);
+            <h3>Cosas {input.color}</h3>
+        </button>
+        </div>);
 }
