3138966376e3ff78ae91f2a56bdba8bca94e0b7f
DarioFD <dfungueirinhoduro3@gmail.com>
2023-08-06 00:53:10 +0200

    Background

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -8,7 +8,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      <main className="">
+      <main className="bg-black h-screen flex flex-row relative justify-center items-center">
+        <div style={{width:"200px" , height:"200px" , borderRadius:"50%" , boxShadow:"0 0 60px 60px #000,0 0 100px 120px #39FF14"}} className='bg-black'></div>
       </main>
       <footer>
       </footer>
