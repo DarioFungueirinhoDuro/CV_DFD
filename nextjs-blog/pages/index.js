c94445191a3d00c17c7795ce6ba4c525519f9a11
DarioFD <dfungueirinhoduro3@gmail.com>
2023-08-06 18:07:51 +0200

    Logo

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -4,9 +4,12 @@ import Background from '../components/Background';
 import LeftMenu from '../components/LeftMenu';
 export default function Home() {
   return (
-    <div className=''>
+    <div>
       <Head>
         <title>CV_DFD</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch&display=swap" rel="stylesheet" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
