a6b568a93055f116d016202ea0819ca26f593689
DarioFD <dfungueirinhoduro3@gmail.com>
2023-08-07 21:07:39 +0200

    WorkingOnGlowing

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
         <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch&family=Dosis:wght@300&display=swap" rel="stylesheet"></link>
         <link rel="icon" href="/favicon.ico" />
+        <link rel='stylesheet' href='../styles/glows.css'></link>
       </Head>
       
       <main className='relative'>
