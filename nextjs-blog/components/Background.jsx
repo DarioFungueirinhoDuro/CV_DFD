8bde804987ceb74afd53b734d65f461c5b7c3988
DarioFD <dfungueirinhoduro3@gmail.com>
2023-08-09 20:30:45 +0200

    BackgroundAnimation

--- nextjs-blog/components/Background.jsx
+++ nextjs-blog/components/Background.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-
 export default function Background(){
-    
     return(
+        
     <div className='absolute bg-transparent w-screen min-h-screen'>
         <div id='backgroundContiner' className="bg-black h-screen flex flex-row relative justify-center items-center z-0 ">
-            <div id='light' style={{width:"200px" , height:"200px" , borderRadius:"50%" , boxShadow:"0 0 60px 60px #000,0 0 100px 120px #39FF14"}} className='bg-black glow'></div>
+            <div id='light' style={{width:"200px" , height:"200px" , borderRadius:"50%" , boxShadow:"0 0 60px 60px #000,0 0 100px 120px #39FF14",}} className='bg-black glow'></div>
             <div id='darkCircle' style={{width:"200px" , height:"200px" , borderRadius:"50%" }} className='bg-black absolute' ></div>
         </div>
     </div>
